feb84d94fb9acc9e08e0ff6cd02403b5b4901b46
Faizan Jan 2 <faizanjan2@Faizans-MacBook-Air.local>
2023-07-15 15:35:38 +0530

    Add a background gradient to the body in all routes

--- includes/frontend/report.php
+++ includes/frontend/report.php
@@ -9,16 +9,16 @@ Test Attempts
 <?php endblock() ?>
 
 <?php startblock('head') ?>
-  <?php superblock() ?>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://ap-development.s3.amazonaws.com/autoproctor.2.7.8.min.css" />
-  <script defer src="https://ap-development.s3.amazonaws.com/autoproctor.2.7.8.min.js"></script>
+<?php superblock() ?>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://ap-development.s3.amazonaws.com/autoproctor.2.7.8.min.css" />
+<script defer src="https://ap-development.s3.amazonaws.com/autoproctor.2.7.8.min.js"></script>
 <?php endblock() ?>
 
 <?php startblock('body') ?>
 
-<body>
+<body class="min-h-screen bg-gradient-to-b from-blue-200 to-white" style="font-family: lato">
     <div class="container mx-auto mt-8 max-w-5xl mx-auto">
         <h1 class="text-3xl font-bold mb-4">Test Attempts Report <?php echo $attemptLabel; ?> </h1>
         <div id="ap-test-report-overview" class="max-w-5xl mx-auto"></div>
@@ -29,28 +29,28 @@ Test Attempts
 </body>
 
 <script>
-  window.addEventListener('load', async function() {
-      const apReportSettings = {
-          tenantId: '<?php echo $clientId; ?>',
-          testAttemptId: '<?php echo $attemptLabel; ?>',
-          hashedTestAttemptId: '<?php echo $hashedTestAttemptId; ?>',
-          domain: "<?php echo $domain; ?>"
-      };
-      try {
-          autoProctorTest = await initAutoProctorReport(apReportSettings);
-          autoProctorTest.renderProctoringSummary({
-              renderOverview: true,
-              proctorOverviewCtrId: "ap-test-report-overview",
-              renderSummaryTable: true,
-              proctorSummaryCtrId: "ap-proctor-summary",
-              userDetails: {
-                  name: "First Last",
-                  email: "useremail@gmail.com",
-              },
-          });
-      } catch (err) {
-          console.log(err);
-      }
-  });
+window.addEventListener('load', async function() {
+    const apReportSettings = {
+        tenantId: '<?php echo $clientId; ?>',
+        testAttemptId: '<?php echo $attemptLabel; ?>',
+        hashedTestAttemptId: '<?php echo $hashedTestAttemptId; ?>',
+        domain: "<?php echo $domain; ?>"
+    };
+    try {
+        autoProctorTest = await initAutoProctorReport(apReportSettings);
+        autoProctorTest.renderProctoringSummary({
+            renderOverview: true,
+            proctorOverviewCtrId: "ap-test-report-overview",
+            renderSummaryTable: true,
+            proctorSummaryCtrId: "ap-proctor-summary",
+            userDetails: {
+                name: "First Last",
+                email: "useremail@gmail.com",
+            },
+        });
+    } catch (err) {
+        console.log(err);
+    }
+});
 </script>
 <?php endblock() ?>
--- includes/frontend/results.php
+++ includes/frontend/results.php
@@ -1,67 +1,78 @@
 <?php include 'base.php' ?>
 
 <?php startblock('title') ?>
-    Test Results
+Test Results
 <?php endblock() ?>
 
 
 <?php startblock('head') ?>
-    <?php superblock() ?>
-    <link rel="stylesheet" href="https://ap-development.s3.ap-south-1.amazonaws.com/socTable.css">
-    <script src="https://ap-development.s3.ap-south-1.amazonaws.com/socTable.js"></script>
+<?php superblock() ?>
+<link rel="stylesheet" href="https://ap-development.s3.ap-south-1.amazonaws.com/socTable.css">
+<script src="https://ap-development.s3.ap-south-1.amazonaws.com/socTable.js"></script>
 <?php endblock() ?>
 
 <?php startblock('body') ?>
-    <body>
-        <div class="container mx-auto mt-8">
-            <h1 class="text-3xl font-bold mb-4">Test Attempts of test <?php echo $test_id; ?> </h1>
-            <div id="ap-test-attempts"></div>
-        </div>
-    </body>
-    <script>
-        const attempts = <?php echo json_encode($results); ?>;
-        window.addEventListener('load', function() {
-            const columns = [
-                    { id: "index", header: "S.No" },
-                    { id: "label" },
-                    { id: "started_at", cell: (props) => {
-                            return props.getValue() === null ? "-" : props.getValue();
-                        }, },
-                    { id: "finished_at", cell: (props) => {
-                            return props.getValue() === null ? "-" : props.getValue();
-                        }, },
-                    {
-                        id: "finished",
-                        header: "Test Completed",
-                        cell: (props) => {
-                            return props.getValue() === "true" ? "Yes" : "No";
-                        },
-                    },
-                    {
-                        id: "report",
-                        header: "Report",
-                        enableSorting: false,
-                        enableGlobalFilter: false,
-                        cell: (props) => {
-                            return `<a class="text-blue-500 hover:underline hover:text-blue-600" href="${props.getValue()}" target="_blank">
+
+<body class="min-h-screen bg-gradient-to-b from-blue-200 to-white" style="font-family: lato">
+    <div class="container mx-auto mt-8">
+        <h1 class="text-3xl font-bold mb-4">Test Attempts of test <?php echo $test_id; ?> </h1>
+        <div id="ap-test-attempts"></div>
+    </div>
+</body>
+<script>
+const attempts = <?php echo json_encode($results); ?>;
+window.addEventListener('load', function() {
+    const columns = [{
+            id: "index",
+            header: "S.No"
+        },
+        {
+            id: "label"
+        },
+        {
+            id: "started_at",
+            cell: (props) => {
+                return props.getValue() === null ? "-" : props.getValue();
+            },
+        },
+        {
+            id: "finished_at",
+            cell: (props) => {
+                return props.getValue() === null ? "-" : props.getValue();
+            },
+        },
+        {
+            id: "finished",
+            header: "Test Completed",
+            cell: (props) => {
+                return props.getValue() === "true" ? "Yes" : "No";
+            },
+        },
+        {
+            id: "report",
+            header: "Report",
+            enableSorting: false,
+            enableGlobalFilter: false,
+            cell: (props) => {
+                return `<a class="text-blue-500 hover:underline hover:text-blue-600" href="${props.getValue()}" target="_blank">
                                     View Report
                                 </a>`;
 
-                        },
-                    },
-                ];
+            },
+        },
+    ];
 
-                const tableData = [];
-                attempts.forEach((d, index) => {
-                    tableData.push([index + 1, d.test_attempt_label,d.started_at, d.finished_at, d.finished, d.report_url]);
-                });
+    const tableData = [];
+    attempts.forEach((d, index) => {
+        tableData.push([index + 1, d.test_attempt_label, d.started_at, d.finished_at, d.finished, d
+            .report_url
+        ]);
+    });
 
-                socTable.initialize("ap-test-attempts", {
-                    columns,
-                    data: tableData,
-                });
-        });
-    </script>
+    socTable.initialize("ap-test-attempts", {
+        columns,
+        data: tableData,
+    });
+});
+</script>
 <?php endblock() ?>
-
-
